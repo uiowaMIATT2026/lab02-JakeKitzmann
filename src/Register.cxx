89907f8a0d85ce4fafee5e6077de8f3d3b070b13
JakeKitzmann <97077033+JakeKitzmann@users.noreply.github.com>
2026-04-09 17:11:22 -0500

    ENH: Arg parse and image read

--- src/Register.cxx
+++ src/Register.cxx
@@ -2,10 +2,28 @@
 
 // itk
 #include <itkImage.h>
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
 
 // C++
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-  std::cout << "Hello World!" << std::endl;
+
+  using PixelType = double;
+  constexpr unsigned int Dimension = 2;
+  using ImageType = itk::Image<PixelType, Dimension>;
+
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " <fixed image> <moving image>" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  auto fixedImage = itk::ReadImage<ImageType>(argv[1]);
+  auto movingImage = itk::ReadImage<ImageType>(argv[2]);
+
+  std::cout << "Images Read" << std::endl;
+
+
+  return EXIT_SUCCESS;
 }
